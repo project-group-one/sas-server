06d8ad22a1cfa7f8bd9403626535659c39a26f20
沈涛 <ygdxdyj@gmail.com>
2019-04-18 23:49:47 +0800

    #bug fix
    add static resource location
    add pic creator

--- src/main/java/com/food/sas/controller/FocalPictureController.java
+++ src/main/java/com/food/sas/controller/FocalPictureController.java
@@ -2,14 +2,20 @@ package com.food.sas.controller;
 
 import com.food.sas.data.dto.BaseResult;
 import com.food.sas.data.dto.FocalPictureDTO;
+import com.food.sas.security.User;
 import com.food.sas.service.IFocalPictureService;
+import com.food.sas.service.IUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 /**
  * @author Created by ygdxd_admin at 2019-01-08 9:36 PM
@@ -22,6 +28,9 @@ public class FocalPictureController {
     @Autowired
     private IFocalPictureService service;
 
+    @Autowired
+    private IUserService userService;
+
     @ApiOperation("查询焦点图")
     @GetMapping
     public BaseResult<?> searchFocalPicture(@RequestParam(required = false) String name, @RequestParam(value = "current", defaultValue = "1") int page,
@@ -33,7 +42,9 @@ public class FocalPictureController {
 
     @ApiOperation("新增焦点图")
     @PostMapping
-    public Mono<?> createFocalPicture(@RequestBody FocalPictureDTO body) {
+    public Mono<?> createFocalPicture(@RequestBody FocalPictureDTO body, Authentication authentication) {
+        System.out.println(((UserDetails) authentication.getPrincipal()).getUsername());
+        body.setCreator(userService.findUserByUsername(((UserDetails) authentication.getPrincipal()).getUsername()).getId());
         service.createFocalPicture(body);
         return Mono.empty();
     }
