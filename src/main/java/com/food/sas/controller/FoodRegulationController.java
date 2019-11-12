9386718c2056e4ff2c90b86894d53b2111351647
沈涛 <ygdxdyj@gmail.com>
2019-11-12 21:52:36 +0800

    #增加食品类别细则

---
+++ src/main/java/com/food/sas/controller/FoodRegulationController.java
@@ -0,0 +1,12 @@
+package com.food.sas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Created by ygdxd_admin at 2019-11-11 8:49 PM
+ */
+@RequestMapping("/food/regulation")
+@RestController
+public class FoodRegulationController {
+}
--- src/main/java/com/food/sas/data/dto/FoodRegulationDTO.java
+++ src/main/java/com/food/sas/data/dto/FoodRegulationDTO.java
@@ -1,5 +1,7 @@
 package com.food.sas.data.dto;
 
+import lombok.Data;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,8 +10,23 @@ import java.io.Serializable;
 /**
  * @author Created by ygdxd_admin at 2019-11-09 5:03 PM
  */
+@Data
 public class FoodRegulationDTO implements Serializable {
 
     private static final long serialVersionUID = -4989432889298728060L;
+
+    private Integer id;
+
+    private String range;
+
+    private String category;
+
+    private String samplingBasis;
+
+    private String demand;
+
+    private String attention;
+
+    private String conclusion;
 
 }
